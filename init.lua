745697390b2127b3d187ad769956b4b72b0d1e9f
frank89722 <mail@frankv.me>
2023-08-08 16:20:27 +0800

    add `smoka7/multicursors` plugin

--- init.lua
+++ init.lua
@@ -27,6 +27,25 @@ return {
     { "chaoren/vim-wordmotion" },
     { "kamykn/spelunker.vim" },
     { "tanvirtin/monokai.nvim" },
+    {
+      "smoka7/multicursors.nvim",
+      event = "VeryLazy",
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'smoka7/hydra.nvim',
+      },
+      opts = {},
+      cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+      keys = {
+        {
+          mode = { 'v', 'n' },
+          '<A-n>',
+          '<cmd>MCstart<cr>',
+          desc = 'Create a selection for selected text or word under the cursor',
+        },
+      },
+    },
+
   },
 
   mappings = {
